74ee738b8b6e102a1496eb78341bcc21e8793400
javalo <lo.cheikhna@gmail.com>
2020-12-07 01:12:03 +0100

    Style stocks

--- robinhood-clone/src/components/Stats.js
+++ robinhood-clone/src/components/Stats.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import './Stats.css'
+
 function Stats() {
+
     return (
         <div className="Stats">
             
+            <div className="Stats-container">
+            
+            <div className="Stats-header">
+                 <p>Stacks</p>
+            </div>
+            <div className="Stats-content">
+                 <div className="Stats-rows">
+                 {/* for our current stocks */}
+                 </div>
+            </div>
+
+
+            <div className="Stats-header">
+                 <p>List</p>
+            </div>
+            <div className="Stats-content">
+                 <div className="Stats-rows">
+                   {/* for stocks we can buy  */}
+                 </div>
+            </div>
+
+
+
+            </div>
+
         </div>
     )
 }
