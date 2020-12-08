acada9f7b911236484728480889706192546f267
javalo <lo.cheikhna@gmail.com>
2020-12-08 14:27:49 +0100

    Popular lists

--- robinhood-clone/src/components/Newsfeed.js
+++ robinhood-clone/src/components/Newsfeed.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React , { useState, useEffect } from "react";
 import LineGraph from "./LineGraph";
 import "./Newsfeed.css";
 import TimeLine from "./TimeLine";
-
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import FaceIcon from '@material-ui/icons/Face';
 const Newsfeed = () => {
+
+    const [popularTopics, setTopics] = useState([
+        "Technology",
+        "Top Movies",
+        "Upcoming Earnings",
+        "Crypto",
+        "Cannabis",
+        "Healthcare Supplies",
+        "Index ETFs",
+        "Technology",
+        "China",
+        "Pharma",
+      ]);
+
+
+      const handleDelete = () => {
+        console.info('You clicked the delete icon.');
+      };
+
   return (
     <div className="Newsfeed">
       <div className="Newsfeed-container">
@@ -17,10 +38,49 @@ const Newsfeed = () => {
             <LineGraph />
             <TimeLine />
           </div>
-
-          <div>
-          </div>
         </div>
+
+        <div className="Newfeed-buying-section">
+            <h2>Buying power</h2>
+            <h2>$5.23</h2>
+          </div>
+
+<div className="Newfeed-market-section">
+<div className="Newfeed-market-box">
+     <p>Cash Management</p>
+     <h1>Happy Black friday</h1>
+</div>
+</div>
+
+
+<div className="Newsfeed-popularlists-section">
+          <div className="Newsfeed-popularlists-intro">
+            <h1>Popular lists</h1>
+            <p>Show More</p>
+          </div>
+
+          <div className="newsfeed_popularlists_badges">
+            {popularTopics.map((topic) => (
+              <Chip className="Topic-badge"
+              avatar={<Avatar
+                src={`https://avatars.dicebear.com/api/human/${topic}.svg`}
+              />} 
+              label={topic}
+             
+              variant="outlined"
+
+
+
+              />
+            ))}
+          </div>
+
+
+
+
+          </div>
+
+
       </div>
     </div>
   );
--- robinhood-clone/src/components/StatsRow.js
+++ robinhood-clone/src/components/StatsRow.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./StatsRow.css";
-import iconChart from "../stock.svg";
+import stock from "../stock.svg";
+import negStock from "../negStock.svg";
 
 function StatsRow(props) {
+  var iconStock=stock;
   const percentage = (
     ((props.price - props.openPrice) / props.openPrice) *
     100
   ).toFixed(2);
+  if(percentage<0) iconStock=negStock
   return (
     <div className="StatsRow">
       <div className="StatsRow-intro">
@@ -14,7 +17,9 @@ function StatsRow(props) {
         <p> {props.share && props.share + " shares"}</p>
       </div>
       <div className="StatsRow-chart">
-        <img src={iconChart} height={16} />
+        
+
+        <img src={iconStock} height={16} />
       </div>
       <div className="StatsRow-numbers">
         <p className="StatsRow-price">{"$" + props.price}</p>
